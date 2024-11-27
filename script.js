4dbd761e78d2d87cc6aa43c85b0299c0a619b974
0513Tim <125227499+0513Tim@users.noreply.github.com>
2024-11-28 03:43:57 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    // 擷取 sessionStorage 的資料
+    const us = sessionStorage.getItem('username');
+    const ps = sessionStorage.getItem('password');
+    // 獲得輸入的值
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if(username === us && password === ps){
+        window.location.href = 'welcome.html';
+    }
+    else if(username === us && password !== ps){
+        alert("密碼錯誤");
+    }
+    else{
+        alert("帳號不存在，請先註冊");
+    }
+}
