6e9ab0433ab876859e865c999902262d43b0bedc
0513Tim <125227499+0513Tim@users.noreply.github.com>
2024-11-29 21:44:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function handleSubmit(event) {
     // 避免瀏覽器重新整理
     event.preventDefault();
     // 擷取 localStorage 的資料
-    // const ul = localStorage.getItem('username');
-    // const pl = localStorage.getItem('password');
+    const ul = localStorage.getItem('username');
+    const pl = localStorage.getItem('password');
     // 擷取 sessionStorage 的資料
     const us = sessionStorage.getItem('username');
     const ps = sessionStorage.getItem('password');
     // 擷取 cookie 的資料
-    // const uc = document.cookie.split(";")[0].slice(9);
-    // const pc = document.cookie.split(";")[1].slice(10);
+    const uc = document.cookie.split(";")[0].slice(9);
+    const pc = document.cookie.split(";")[1].slice(10);
     // 獲得輸入的值
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -26,8 +26,6 @@ function handleSubmit(event) {
     else{
         alert("帳號不存在，請先註冊");
     }
-    // username="";
-    // password="";
 }
 const form = document.getElementById('login');
 form.addEventListener('submit',handleSubmit);
