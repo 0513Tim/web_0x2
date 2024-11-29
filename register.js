18fc5be848ccb565c7221ccfb8d4e7a94169b08f
0513Tim <125227499+0513Tim@users.noreply.github.com>
2024-11-29 21:36:59 +0800

    Update register.js

--- register.js
+++ register.js
@@ -3,13 +3,18 @@ function handleSubmit(event){
     // 取得輸入資料
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    // 將資料存入 sessionStorage
+    // 將資料存入 local Storage
+    //localStorage.setItem('username', username);
+    //localStorage.setItem('password', password);
+   
+    // 將資料存入 session Storage
     sessionStorage.setItem('username', username);
     sessionStorage.setItem('password', password);
-    // console.log(username);
-    // console.log(password);  
+    // 將資料存入 cookie
+    // document.cookie = "username=" + username;
+    // document.cookie = 'password=' + password;
+
     window.location.href = 'index.html';
-    
 }
 const form = document.getElementById('login');
-form.addEventListener('submit',handleSubmit)
+form.addEventListener('submit',handleSubmit);  
